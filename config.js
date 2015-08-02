a398a733ae2c25ae342c09eb51797b22d827b1d9
1bm <ben.mcc@live.com>
2015-08-02 16:41:50 -0700

    bug fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	"database": "mongodb://root:test123@ds037817.mongolab.com:37817/userstory",
-	"port": process.PORT || 3000,
+	"port": process.env.PORT || 3000,
 	"secretKey": "YourSecretKey"
 };
